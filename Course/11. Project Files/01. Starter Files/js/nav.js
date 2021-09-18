b9428906170423b6f38ad0ff805966ce45bbf0dd
agx540 <alexander.geltermair@live.com>
2021-09-18 17:16:06 +0200

    includeHTML on load

--- Course/11. Project Files/01. Starter Files/js/nav.js
+++ Course/11. Project Files/01. Starter Files/js/nav.js
@@ -1,4 +1,4 @@
-
+//https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 function decorateNavigationStyle() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
